bc41268a371fa4676dd5f8a3c5aa49bbca25d0df
Brent Watson <brentmwatson@gmail.com>
2016-11-05 14:42:25 -0400

    lists created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #not sure what a helper method is
+  helper_method :current_user
+
+private
+  #Identify current user by Sql query and assign to object @user
+  def current_user
+    # @user assigned User by user_id if/when session is the same
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,40 +1,59 @@
 class ListsController < ApplicationController
 
-  #if no session
-  #two buttons
-  #if session exist
-  #show list
   def index
+    # HAS TEMPLATE reders ALL list
     if session[:user_id]
+      @lists=List.all
       #render lists.html.erb
       render :lists
     else
-      # render index.html.erb
+      # render home page
       render :index
     end
   end
 
-  def new
-    @list=List.new
-    #create new form here
+
+  def show
+    #HAS TEMPLATE renders SINGLE list
   end
 
-  def create
-    @list=List.new(list_params)
-    if @list.save
+  def new #displays the form for NEW record
+    #HAS TEMPLATE
+    @list=List.new
 
-      flash[:notice]="List created successfully."
-      else
+  end
+
+  def create #Saves new record changes
+    #SAVES and REDIRECTS
+    @list=List.new(list_params) #assign object based on params
+              #@list.user = current_user #only the list of the current user
+    if @list.save #if save occures
+      flash[:notice]="List created successfully." #flash for user
+              #@list=current_user.list.new(list_params)
+      # ****might nedd to be moved to before if^
+      redirect_to :root
+    else
+        render :new
     end
 
   end
 
-  # def edit
+
+  # def edit #Displays form for EXISTING record
+  #   #HAS TEMPLATE
   # end
 
-  # def update
+
+  # def update #saves changes to EXISTING record
+  #   #SAVES AND REDIRECTS
   # end
 
-  def destroy
+
+  def destroy #DESTROYS and REDIRECTS
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:listname)
   end
 end
